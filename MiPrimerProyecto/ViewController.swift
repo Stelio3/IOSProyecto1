6690f030bc0ed2db707cd5d9ef997325015f5bca
Stelio3 <pablohernandezjimenez@gmail.com>
2018-03-22 12:53:23 +0100

    Primer Commit

--- MiPrimerProyecto/ViewController.swift
+++ MiPrimerProyecto/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblPrueba:UILabel?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblPrueba?.text = "Hello!"
     }
 
     override func didReceiveMemoryWarning() {
