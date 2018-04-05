83e024f50905de6ef35bce693030b496f68dbd35
Stelio3 <pablohernandezjimenez@gmail.com>
2018-04-05 14:10:09 +0200

    cuarto commit

---
+++ MiPrimerProyecto/MiCelda1.swift
@@ -0,0 +1,24 @@
+//
+//  MiCelda1.swift
+//  MiPrimerProyecto
+//
+//  Created by PABLO HERNANDEZ JIMENEZ on 5/4/18.
+//  Copyright © 2018 PABLO HERNANDEZ JIMENEZ. All rights reserved.
+//
+
+import UIKit
+
+class MiCelda1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
